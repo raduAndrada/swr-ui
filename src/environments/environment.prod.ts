98d9e8560c4ae72caf4d9016619923bd097d2813
andrada radu <16ey9jeatv7xpoun0nc5jnu98ix790@bots.bitbucket.org>
2024-05-27 13:51:32 +0300

    new ip

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,17 +1,17 @@
 export const environment = {
     production: true,
     staticData: {
-      serverPort: "http://ec2-18-192-42-11.eu-central-1.compute.amazonaws.com:8900/api/",
+      serverPort: "http://ec2-18-194-242-152.eu-central-1.compute.amazonaws.com:8900/api/",
       trendingApi: "trending-blocks",
       teamApi: "team-blocks",
       imagesApi: "image-blocks"
     },
     menu: {
-      serverPort: "http://ec2-18-192-42-11.eu-central-1.compute.amazonaws.com:8901/api/",
+      serverPort: "http://ec2-18-194-242-152.eu-central-1.compute.amazonaws.com:8901/api/",
       menuApi: "menu",
     },
     commonicationChannel: {
-      serverPort: "http://ec2-18-192-42-11.eu-central-1.compute.amazonaws.com:8902/api/",
+      serverPort: "http://ec2-18-194-242-152.eu-central-1.compute.amazonaws.com:8902/api/",
       emailApi: "communication-channel/email/reservations"
     }
   };
