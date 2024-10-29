edcbd89e69fad43f91da1c76f54caeb91ca78c20
andrada radu <16ey9jeatv7xpoun0nc5jnu98ix790@bots.bitbucket.org>
2024-10-29 07:55:55 +0200

    test: more nginx config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 export const environment = {
     production: true,
     menu: {
-      serverPort: "https://bistrogram.ro/api/",
+      serverPort: "https://www.bistrogram.ro/api/",
       menuApi: "menu",
     },
     communicationChannel: {
-      serverPort: "https://bistrogram.ro/api/",
+      serverPort: "https://www.bistrogram.ro/api/",
       emailApi: "communication-channel/email/reservations"
     }
   };
